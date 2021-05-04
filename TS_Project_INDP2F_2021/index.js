087ae1bde4e7df03a5c1f72704d5bfc226c8f336
MSalaani <melek.salaani@supcom.tn>
2021-05-04 14:54:10 +0100

    initial commit

---
+++ TS_Project_INDP2F_2021/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, '/styles')));
+app.use(express.static(path.join(__dirname, '/pages')));
+app.use(express.static(path.join(__dirname, '/imgs')));
+app.use(express.static(path.join(__dirname, '/')));
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/number_guesser', function (req, res) {
+	res.sendFile(path.join(__dirname, '/pages/number_guesser.html'));
+});
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`)
+})
