3ebefc50d05a8ad77e498334c5abfa50787920f7
MSalaani <melek.salaani@supcom.tn>
2021-05-16 19:21:18 +0100

    Re-structuration

--- TS_Project_INDP2F_2021/scripts/index.js
+++ TS_Project_INDP2F_2021/scripts/index.js
@@ -3,16 +3,16 @@ const path = require('path');
 const app = express()
 const port = 3000
 
-app.use(express.static(path.join(__dirname, '/styles')));
-app.use(express.static(path.join(__dirname, '/pages')));
-app.use(express.static(path.join(__dirname, '/imgs')));
+app.use(express.static(path.join(__dirname, '../styles')));
+app.use(express.static(path.join(__dirname, '../pages')));
+app.use(express.static(path.join(__dirname, '../imgs')));
 app.use(express.static(path.join(__dirname, '/')));
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.get('/number_guesser', function (req, res) {
-	res.sendFile(path.join(__dirname, '/pages/number_guesser.html'));
+	res.sendFile(path.join(__dirname, '../pages/number_guesser.html'));
 });
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
